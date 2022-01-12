22ee3598ead7c0cf9e046f2b5bd5addb3f79e483
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-12 21:51:38 +0100

    222 uniklany emial dla każdego usera

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -30,6 +30,16 @@ async function handler(req, res) {
 
   const db = client.db();
 
+  // sprawdzanie czuy user już istenije - sprawdzanie unikalności emaili
+  const existingUser = await db.collection("users").findOne({ email: email }); //key-value pairs jak argument do szukania dokuemntu/rekordu bazy
+  //   albo undefined albo userObject z bazy
+  if (existingUser) {
+    // user już istneije w bazie
+    res.status(422).json({ message: "User exists already" });
+    client.close(); //pamietać o zamykaniu połaczenia
+    return;
+  }
+
   const hashedPassword = await hashPassword(password); // bo zwraca promise dlatego bez tego było ibket w bazie a nie string
 
   //zwraca promsia
@@ -39,6 +49,7 @@ async function handler(req, res) {
   });
 
   res.status(201).json({ message: "Created user!" });
+  client.close(); //pamietać o zamykaniu połaczenia
 }
 
 export default handler;
