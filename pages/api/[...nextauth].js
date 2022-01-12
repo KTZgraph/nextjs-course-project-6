e84793fce5e04d5c7175063a48a1c2cfe7593286
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-12 22:42:49 +0100

    223 [...nextauth].js Credentials Auth Provide

--- pages/api/[...nextauth].js
+++ pages/api/[...nextauth].js
@@ -11,6 +11,17 @@ import { connectToDatabase } from "../../lib/db";
 // PAMIETAC O execute - zwraca nową funkcję a handler function it needs to beacause it is stil and API route , a api route zawsze musi export funkcji
 // przyjmuje konfiguracyjny obiekt
 export default NextAuth({
+  // żeby mieć pewnosć, ze obiekt JWT został utworzony https://next-auth.js.org/configuration/options
+  // w tym obiekcie mozna skonfigurowac jak authenticated user willbe managed
+  //sesje można zapisac w bazie danych  https://next-auth.js.org/configuration/options#session
+  // ale for credentials based authentication  for this Provider [Providers.Credentails] you must use [jwt] and set this to true
+  //   jwt z uatomatu były ustawiony na true jeśli nie wyspecyfikowało się bazy danych and we didn't specife database,
+  // because we handled all database access manualy anyways with this specyfic providers,a el explicit ustawianie [jwt:true] też jest spoczko
+  //   jak sie uzywa innych providerów to mozna ustawić sesje, ale to zależy już od "Authenticaed Providers" z dokumentacji np jak używac się Email to trzeba w bazie zapisac magiczny link i w tedy nie trzeba używac jwt
+  session: {
+    jwt: true,
+  },
+
   providers: Providers.Credentails({
     //   nie chęc żeby next-aut generował za mnie form bo już mam swój
     //   credentails: {
@@ -38,16 +49,17 @@ export default NextAuth({
 
       if (!isValid) {
         //nieprawidłowe hasło podane
+        client.close(); // pamietać o zamykaniu połaczenia do naszej bazy
         throw new Error("Could not  log you in!");
       }
+
+      client.close(); // pamietać o zamykaniu połaczenia do naszej bazy
 
       //user jest ok - zwracamy obiekt - dajemy znać NextAuth({ że autoryzacja sie powiodła - user jest zalogowany
       //   obiket będzie zakodowany w JSON Web Token
       // nie powinniśmy przekazywac całego obiketu usera, ponieważ nie chcemy dodać hasła nawet jeśli jest zahaszowane to i tak we don;t want to expose to the client
       return { email: user.email }; //to jest obiekt który zwracamy i któy będzie zakodaowany w json web token
-    //   zeby mieć pewnosc że obiekt JWT jest utworzony musimy iśc do konfiguracji obiekt NextAuth
-
-      client.close(); // pamietać o zamykaniu połaczenia do naszej bazy
+      //   zeby mieć pewnosc że obiekt JWT jest utworzony musimy iśc do konfiguracji obiekt NextAuth
     },
   }),
 });
