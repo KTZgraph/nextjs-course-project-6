e00118a91e430e8f4a22662d6f8b5bfb1597c5e1
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-12 23:59:56 +0100

    DZIALA w zlej sciezce plik niezauwazylam

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,32 +1,34 @@
-import { useState, useRef } from "react";
-import { signIn } from "next-auth/client";
-// https://next-auth.js.org/getting-started/client
-// import { signIn } from "next-auth/react";
+import { useState, useRef } from 'react';
+import { signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
 
-import classes from "./auth-form.module.css";
+import classes from './auth-form.module.css';
 
-// funckja rejestracji - można do innego pliku wrzucić
 async function createUser(email, password) {
-  const response = await fetch("/api/auth/signup", {
-    method: "POST",
+  const response = await fetch('/api/auth/signup', {
+    method: 'POST',
     body: JSON.stringify({ email, password }),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
 
-  const data = await response.json(); // też zwraca Promise
+  const data = await response.json();
+
   if (!response.ok) {
-    throw new Error(data.message || "Somethin wen wrong");
+    throw new Error(data.message || 'Something went wrong!');
   }
+
+  return data;
 }
 
 function AuthForm() {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
-  const [isLogin, setIsLogin] = useState(true);
 
-  // przełączanie zaloguj / zarejestruj
+  const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
+
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
@@ -37,36 +39,28 @@ function AuthForm() {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
-    // dodać walidacje na froncie, zeby user od razu coś widział, ale jej nie ufać bo po stronie klienta
+    // optional: Add validation
 
     if (isLogin) {
-      //  logowanie tu NIE WSYLAM requesta do api I NIE POWINNAM tego robić
-      // pierwszy argument to provider - bo mozemy mieć kilka providerów w tej samej aplikacji
-
-      // {redirect: false}
-      // drugi argument to obiekt konfiguracji - false bo już mamy swoją stronę rejestracji linia [...nextauth].js- throw ne Error('No user found')
-      //  bez tego by default when authentication failed enxtjs will redirect us to another page to an error page it is something we
-      // could utilize we can the set up such an error page ans then we can configure that error page [dokumentacj providers https://next-auth.js.org/]
-      // ale zwyczajowo pozostawia się usra na stronie na której jest i na tej samej stronie pokazuje się error message BEZ przekierowań
-      //kolejny atrybut obiektu konfiguracji to credentails data które chcemy wysłać do backendu, ten obiekt jest w [NextAuth.authorize(credentiasl) funckji] jako argument
-
-      //zwraca promisa zawsze nawet jak mamy błąd na backendzie w kodzie autentykacji, obiekt będzie mieć informacje o błedzie
-      // jak nie ma błedu to result jest dalej obiektem tylko bez infomacji o błedzie, więc nigdy nie zwróci błedu tylko content obiektu będzie inny
-      const result = await signIn("credentials", {
+      console.log("TUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU")
+      const result = await signIn('credentials', {
         redirect: false,
         email: enteredEmail,
         password: enteredPassword,
       });
 
-      console.log(result);
+      if (!result.error) {
+        // set some auth state
+        router.replace('/profile');
+      }
+      console.log("result.error")
+      console.log(result.error)
+
     } else {
-      // rejestracja tu trzeba do api wsysyłać requesta
       try {
         const result = await createUser(enteredEmail, enteredPassword);
-        // user was created  - notifkacja
         console.log(result);
       } catch (error) {
-        // jakiś błąd przy rejestracji - notifkacja
         console.log(error);
       }
     }
@@ -74,29 +68,29 @@ function AuthForm() {
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required ref={emailInputRef} />
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor="password">Your Password</label>
+          <label htmlFor='password'>Your Password</label>
           <input
-            type="password"
-            id="password"
+            type='password'
+            id='password'
             required
             ref={passwordInputRef}
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          <button>{isLogin ? 'Login' : 'Create Account'}</button>
           <button
-            type="button"
+            type='button'
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? "Create new account" : "Login with existing account"}
+            {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
         </div>
       </form>
