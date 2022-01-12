f735155d00522b892e135a1b582b755517bf79b2
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-12 21:47:15 +0100

    221 rejestracja do bbazy useraz z form

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,7 +1,26 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useState, useRef } from "react";
+
+import classes from "./auth-form.module.css";
+
+// funckja rejestracji - można do innego pliku wrzucić
+async function createUser(email, password) {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json(); // też zwraca Promise
+  if (!response.ok) {
+    throw new Error(data.message || "Somethin wen wrong");
+  }
+}
 
 function AuthForm() {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
 
   // przełączanie zaloguj / zarejestruj
@@ -9,26 +28,54 @@ function AuthForm() {
     setIsLogin((prevState) => !prevState);
   }
 
+  async function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // dodać walidacje na froncie, zeby user od razu coś widział, ale jej nie ufać bo po stronie klienta
+
+    if (isLogin) {
+      // tylko logowanie
+    } else {
+      // rejestracja
+      try {
+        const result = await createUser(enteredEmail, enteredPassword);
+        // user was created  - notifkacja
+        console.log(result);
+      } catch (error) {
+        // jakiś błąd przy rejestracji - notifkacja
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,6 +2,11 @@ import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
+  // powinno tworyzć użytkownika tylko na metodę POST
+  if (req.method !== "POST") {
+    return;
+  }
+
   //extract incoming data
   const data = req.body;
   const { email, password } = data;
@@ -13,31 +18,27 @@ async function handler(req, res) {
     password.trim().length < 7
   ) {
     // invalid data
-    res
-      .status(422)
-      .json({
-        message:
-          "Invalid input - password shoul also be at least 7 characters long",
-      });
+    res.status(422).json({
+      message:
+        "Invalid input - password shoul also be at least 7 characters long",
+    });
     return;
   }
 
-
   // connect to database
-  const client = connectToDatabase();
+  const client = await connectToDatabase();
 
   const db = client.db();
 
-  const hashedPassword = hashPassword(password);
+  const hashedPassword = await hashPassword(password); // bo zwraca promise dlatego bez tego było ibket w bazie a nie string
 
   //zwraca promsia
   const result = db.collection("users").insertOne({
-      email,
-      password: hashedPassword //nie zapisać hasła jako plaintext
+    email,
+    password: hashedPassword, //nie zapisać hasła jako plaintext
   });
 
-
-  res.status(201).json({message: 'Created user!'});
+  res.status(201).json({ message: "Created user!" });
 }
 
 export default handler;
