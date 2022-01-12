2eb7c1a135a3512f08cb051f463f19baac9c4e07
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-12 23:22:19 +0100

    npm uninstall next-auth

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,4 +1,7 @@
 import { useState, useRef } from "react";
+import { signIn } from "next-auth/client";
+// https://next-auth.js.org/getting-started/client
+// import { signIn } from "next-auth/react";
 
 import classes from "./auth-form.module.css";
 
@@ -37,9 +40,27 @@ function AuthForm() {
     // dodać walidacje na froncie, zeby user od razu coś widział, ale jej nie ufać bo po stronie klienta
 
     if (isLogin) {
-      // tylko logowanie
+      //  logowanie tu NIE WSYLAM requesta do api I NIE POWINNAM tego robić
+      // pierwszy argument to provider - bo mozemy mieć kilka providerów w tej samej aplikacji
+
+      // {redirect: false}
+      // drugi argument to obiekt konfiguracji - false bo już mamy swoją stronę rejestracji linia [...nextauth].js- throw ne Error('No user found')
+      //  bez tego by default when authentication failed enxtjs will redirect us to another page to an error page it is something we
+      // could utilize we can the set up such an error page ans then we can configure that error page [dokumentacj providers https://next-auth.js.org/]
+      // ale zwyczajowo pozostawia się usra na stronie na której jest i na tej samej stronie pokazuje się error message BEZ przekierowań
+      //kolejny atrybut obiektu konfiguracji to credentails data które chcemy wysłać do backendu, ten obiekt jest w [NextAuth.authorize(credentiasl) funckji] jako argument
+
+      //zwraca promisa zawsze nawet jak mamy błąd na backendzie w kodzie autentykacji, obiekt będzie mieć informacje o błedzie
+      // jak nie ma błedu to result jest dalej obiektem tylko bez infomacji o błedzie, więc nigdy nie zwróci błedu tylko content obiektu będzie inny
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+
+      console.log(result);
     } else {
-      // rejestracja
+      // rejestracja tu trzeba do api wsysyłać requesta
       try {
         const result = await createUser(enteredEmail, enteredPassword);
         // user was created  - notifkacja
