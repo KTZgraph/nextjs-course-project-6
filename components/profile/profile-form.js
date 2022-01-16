65f7d23c18d719925da4687cc623074a6bb771e1
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-16 19:13:28 +0100

    234 wysyłanie danych do zabezpieczonego API

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,35 @@
-import classes from './profile-form.module.css';
+import { useRef } from "react";
+import classes from "./profile-form.module.css";
 
-function ProfileForm() {
+function ProfileForm(props) {
+  const oldPasswordRef = useRef();
+  const newPasswordRef = useRef();
+
+  // zmiana hasła ze strony klienta
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredOlPassword = oldPasswordRef.current.value;
+    const enteredNewPassword = newPasswordRef.current.value;
+    // można dac walidacje po stronie klienta zeby user widział co zrobił źle, ale prawdziwa pewna walidacja po stronie serwera
+
+    // wysyłanie danych do komponentu rodzica
+    // funcja props z rodzica
+    props.onChangePassword({
+      oldPassword: enteredOlPassword,
+      newPassword: enteredNewPassword,
+    });
+  }
+  
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" ref={oldPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    //gdy jesteśmy w developerskim serwerze
+    return {
+      reactStrictMode: true,
+      env: {
+        mongodb_username: "testowyuser",
+        mongodb_password: "c9I0SWOqoAm3GZx2",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "auth-demo",
+      },
+    };
+  }
+
+  // gdy jestesmy w produckji
+  return {
+    reactStrictMode: true,
+    env: {
+      mongodb_username: "myuserproduction",
+      mongodb_password: "myuserproductionpasssowrd",
+      mongodb_clustername: "culstermonodb",
+      mongodb_database: "db",
+    },
+  };
+};
