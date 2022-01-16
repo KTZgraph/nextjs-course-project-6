739191a1ed9e34ac6b5cc3654eeab5be313d9b13
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-16 16:04:20 +0100

    225 import { useSession } from "next-auth/client";

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,26 +1,41 @@
-import Link from 'next/link';
+import Link from "next/link";
+// do pokazywania widoków tylko dla zalogowanych userów - useSession hook gdy używanmy fukcyjncyh komponentów Reacta
+import { useSession } from "next-auth/client";
 
-import classes from './main-navigation.module.css';
+import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
+  const [session, loading] = useSession(); //takie dane zwraca ten hook
+
   return (
     <header className={classes.header}>
-      <Link href='/'>
+      <Link href="/">
         <a>
           <div className={classes.logo}>Next Auth</div>
         </a>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {/* pokaż login link tylko gdy nie ma sesji i nie ładujemy*/}
+          {!session && !loading && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+
+          {/* pokazać profil tylko gdy useris authenticated */}
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+
+          {/* wylogowanie pokazać tylko gdy mamy sesję */}
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
